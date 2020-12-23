e06102a6bc48467a4248803b8940ce934fef49d8
Katherine O'Halloran <kohalloran1215@gmail.com>
2020-12-22 19:12:01 -0600

    Final overview commit until formatting changes, merge with master

--- co_summary_tests.py
+++ co_summary_tests.py
@@ -4,6 +4,9 @@ from DataBase import DataBase
 # instantiate DataBase class
 fin_db = DataBase()
 
+# set company selected 
+fin_db.set_co_selected("MSFT")
+
 # call eps method
 fin_db.get_eps("MSFT")
 
@@ -14,24 +17,28 @@ print("-"*100)
 print("MSFT EPS Results: ")
 print(msft_eps)
 
-
 ## Test get_co_overview method
-msft_sector, msft_industry, msft_market_cap, msft_ebita, msft_pe_ratio =  fin_db.get_co_overview("MSFT")
+fin_db.get_co_overview("MSFT")
 
 print("-"*100)
 print("MSFT Company Overview: ")
 
 print("SECTOR: ")
+msft_sector = fin_db.co_sector["MSFT"]
 print(msft_sector)
 
 print("INDUSTRY: ")
+msft_industry = fin_db.co_industry["MSFT"]
 print(msft_industry)
 
 print("MARKET CAP: ")
-print(msft_market_cap)
+msft_mkt_cap = fin_db.co_market_cap["MSFT"]
+print(msft_mkt_cap)
 
 print("EBITA: ")
+msft_ebita = fin_db.co_ebita["MSFT"]
 print(msft_ebita)
 
 print("PE RATIO: ")
+msft_pe_ratio = fin_db.co_pe_ratio["MSFT"]
 print(msft_pe_ratio)
